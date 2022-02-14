c2a5fc6193ff2a64eba2ba14ff43d00309a7013e
Yago Dias <yago.dias1409@gmail.com>
2022-02-14 20:49:23 -0300

    Adicionar arquivos de projeto.

---
+++ calculadora/Program.cs
@@ -0,0 +1,31 @@
+﻿
+class calc
+{
+    static void Main()
+    {
+        Console.WriteLine("First number??");
+        var firstNum = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Wich operation??");
+        var operation = Console.ReadLine();
+
+        Console.WriteLine("Second number");
+        int secondNum = int.Parse(Console.ReadLine());
+
+        switch (operation)
+        {
+            case "plus":
+                Console.WriteLine(firstNum + secondNum);
+                break;
+            case "less":
+                Console.WriteLine(firstNum - secondNum);
+                break;
+            case "multi":
+                Console.WriteLine(firstNum * secondNum);
+                break;
+            case "div":
+                Console.WriteLine(firstNum / secondNum);
+                break;
+        }
+    }
+}
