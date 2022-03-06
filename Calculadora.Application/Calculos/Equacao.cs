26afbd4fc708b2ada1ac9a91b03af6bdb145bfc5
Yago Dias <yago.dias1409@gmail.com>
2022-03-06 10:16:34 -0300

    Adicionando testes

--- Calculadora.Application/Calculos/Equacao.cs
+++ Calculadora.Application/Calculos/Equacao.cs
@@ -12,11 +12,16 @@ namespace Calculadora.Application.Calculos
             var b = InputHelper.GetConsoleInput<double>("Digite B: ");
             var c = InputHelper.GetConsoleInput<double>("Digite C: ");
 
-            var result = new QuadraticResult(a, b, c);
+            var result = Calcular(a, b, c);
 
             Console.WriteLine("Resultado:\n\tx1 {0}\n\tx2 {1}", result.X1, result.X2);
 
             return result;
+        }
+
+        public QuadraticResult Calcular(double a, double b, double c)
+        {
+            return new QuadraticResult(a, b, c);
         }
     }
 }
---
+++ Calculadora.Testes/Application/EquacaoTestes.cs
@@ -0,0 +1,24 @@
+﻿using Calculadora.Application.Calculos;
+using NUnit.Framework;
+
+namespace Calculadora.Testes.Application
+{
+    internal class EquacaoTestes
+    {
+        [Test]
+        [TestCase(4, -3, 1, -1, -12)]
+        [TestCase(3, -3, 3, 0, -27)]
+        public void Deve_Calcular(double Expected_X1, double Expected_X2, double a, double b, double c)
+        {
+            // Arrange
+            var eq = new Equacao();
+
+            // Act
+            var result = eq.Calcular(a, b, c);
+
+            // Assert
+            Assert.AreEqual(Expected_X1, result.X1);
+            Assert.AreEqual(Expected_X2, result.X2);
+        }
+    }
+}
